6ed5f621a0d1e8091e7f9e9426b1f301ba5d8913
peterhurford <peter@peterhurford.com>
2017-01-06 11:16:20 -0600

    More cores

--- rfm.py
+++ rfm.py
@@ -1,71 +1,78 @@
 from collections import Counter
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from multiprocessing import Pool
+from vowpal_platypus import split_file
 import time
 
-filename = 'data/trans10m.csv'
-print 'Starting...'
-num_lines = sum(1 for line in open(filename))
-print '...File length is {}'.format(num_lines)
-start = datetime.now()
-print '...Processing file starting at ' + str(start)
-with open(filename, 'r') as filehandle:
-    i = 0
-    curr_done = 0
-    filehandle.readline() # Throw out header
-    customers = {}
-    while True:
-        item = filehandle.readline()
-        if not item:
-            break
-        item = item.split(',')
-        idx = int(item[0])
-        data = {
-            'date': datetime.strptime(item[6], '%Y-%m-%d'),
-            'quantity': float(item[9]),
-            'amount': float(item[10])
+def run_core(filename):
+    print '{} Starting...'.format(filename)
+    num_lines = sum(1 for line in open(filename))
+    print '{}: ...File length is {}'.format(filename, num_lines)
+    start = datetime.now()
+    print '{}: ...Processing file starting at '.format(filename) + str(start)
+    with open(filename, 'r') as filehandle:
+        i = 0
+        curr_done = 0
+        customers = {}
+        filehandle.readline() # Throw out header (TODO: Remove)
+        while True:
+            item = filehandle.readline()
+            if not item:
+                break
+            item = item.split(',')
+            idx = int(item[0])
+            data = {
+                'date': datetime.strptime(item[6], '%Y-%m-%d'),
+                'quantity': float(item[9]),
+                'amount': float(item[10])
+            }
+            if idx not in customers.keys():
+                customers[idx] = [data]
+            else:
+                customers[idx].append(data)
+            i += 1
+            if i % 1000000 == 0:
+                now = datetime.now()
+                speed = (now - start).total_seconds() * 1000 / float(i)
+                lines_remaining = num_lines - i
+                remaining_time = time.strftime('%H:%M:%S', time.gmtime(speed * lines_remaining / 1000.0))
+                message = '{filename}: {i} lines done in {time} ({done}%), {speed}ms per line, {remain} remaining'
+                print message.format(filename=filename,
+                                     i=i,
+                                     time=str(now - start),
+                                     done=str(int(i / float(num_lines) * 100)),
+                                     speed=str(speed),
+                                     remain=remaining_time)
+
+    print '{} Processing raw RFM data...'.format(filename)
+    rfm_customer = {}
+    ending_date = datetime(2013, 7, 27, 0, 0)
+    one_year_ago = ending_date - relativedelta(years=1)
+    for idx, customer in customers.iteritems():
+        rfm_customer[idx] = {
+            'recency': ending_date - reduce(lambda x, y: x if x > y else y, map(lambda x: x['date'], customer)),
+            'frequency': len(filter(lambda x: x['date'] > one_year_ago, customer)),
+            'monetization': sum(map(lambda x: x['amount'], customer)) / float(len(customer))
         }
-        if idx not in customers.keys():
-            customers[idx] = [data]
-        else:
-            customers[idx].append(data)
-        i += 1
-        if i % 1000000 == 0:
-            now = datetime.now()
-            speed = (now - start).total_seconds() * 1000 / float(i)
-            lines_remaining = num_lines - i
-            remaining_time = time.strftime('%H:%M:%S', time.gmtime(speed * lines_remaining / 1000.0))
-            message = '{i} lines done in {time} ({done}%), {speed}ms per line, {remain} remaining'
-            print message.format(i=i,
-                                 time=str(now - start),
-                                 done=str(int(i / float(num_lines) * 100)),
-                                 speed=str(speed),
-                                 remain=remaining_time)
 
-print 'Processing raw RFM data...'
-rfm_customer = {}
-ending_date = datetime(2013, 7, 27, 0, 0)
-one_year_ago = ending_date - relativedelta(years=1)
-for idx, customer in customers.iteritems():
-    rfm_customer[idx] = {
-        'recency': ending_date - reduce(lambda x, y: x if x > y else y, map(lambda x: x['date'], customer)),
-        'frequency': len(filter(lambda x: x['date'] > one_year_ago, customer)),
-        'monetization': sum(map(lambda x: x['amount'], customer)) / float(len(customer))
-    }
+    print '{} Converting to RFM score...'.format(filename)
+    def ordinal_score(data, key):
+        score = sorted(zip(map(lambda x: x[key], data.values()), data.keys()))
+        return Counter(dict(map(lambda x: (x[1][1], x[0]), zip(range(len(score)), score))))
 
-print 'Converting to RFM score...'
-def ordinal_score(data, key):
-    score = sorted(zip(map(lambda x: x[key], data.values()), data.keys()))
-    return Counter(dict(map(lambda x: (x[1][1], x[0]), zip(range(len(score)), score))))
+    frequency_scores = ordinal_score(rfm_customer, 'frequency')
+    recency_scores = ordinal_score(rfm_customer, 'recency')
+    monetization_scores = ordinal_score(rfm_customer, 'monetization')
+    rfm_scores = frequency_scores + recency_scores + monetization_scores
+    return rfm_scores
 
-frequency_scores = ordinal_score(rfm_customer, 'frequency')
-recency_scores = ordinal_score(rfm_customer, 'recency')
-monetization_scores = ordinal_score(rfm_customer, 'monetization')
-
-rfm_scores = frequency_scores + recency_scores + monetization_scores
+filenames = split_file('data/transactions.csv', num_cores=40, header=True)
+pool = Pool(40)
+rfm_scores_list = pool.map(run_core, filenames)
+rfm_scores = {}
+[rfm_scores.update(scores) for scores in rfm_scores_list]
 customers_sorted_by_rfm = map(lambda x: x[0], sorted(rfm_scores.items(), key=lambda x: x[1]))
-
-
 print 'Awaiting further commands...'
 import pdb
 pdb.set_trace()
